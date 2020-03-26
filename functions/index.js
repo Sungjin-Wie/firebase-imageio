eafb244349bdc871b1b4384bbd6f48b288bb6a36
Sungjin-Wie <tmdwls1203@gmail.com>
2020-03-26 19:15:34 +0900

    0326

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var storageRouter = require('./storage');
 var busboyRouter = require('./busboy');
 var admin = require('firebase-admin');
 
@@ -35,21 +34,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', busboyRouter);
-// app.use('/storage', storageRouter);
 
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
@@ -60,10 +55,3 @@ const api = functions.https.onRequest(app);
 module.exports = {
     api
 };
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
